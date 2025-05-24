c2f71ad451e6212921718b6a6c04e22cbe644c24
hosono kotaro <hosono1985@gmail.com>
2025-05-24 21:12:31 +0900

    Copy apps (#2)
    * feat: Add line-bot sdk
    
    * fix: Copy code
    
    * fix: Remove jsonc
    
    * fix: post method

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,56 @@
+import {
+  messagingApi,
+  middleware as lineMiddleware,
+  WebhookRequestBody,
+} from '@line/bot-sdk'
 import { Hono } from 'hono'
 
-const app = new Hono()
+import { getThisMonthWorkList, toJST } from './lib'
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+type HonoEnv = {
+  Bindings: {
+    CHANNEL_SECRET: string
+    CHANNEL_ACCESS_TOKEN: string
+  }
+}
+
+const app = new Hono<HonoEnv>()
+
+app.use('/webhook', async (c, next) => {
+  lineMiddleware({ channelSecret: c.env.CHANNEL_SECRET })
+  await next()
 })
 
-export default app
+app.post('/webhook', async (c) => {
+  const { MessagingApiClient } = messagingApi
+
+  const client = new MessagingApiClient({
+    channelAccessToken: c.env.CHANNEL_ACCESS_TOKEN,
+  })
+
+  const now = toJST(new Date())
+  const schedule = getThisMonthWorkList(now)
+  const today = schedule[now.getDate() - 1]
+  const tomorrow = schedule[now.getDate()]
+  const gomiUrl =
+    'https://www.city.kita.tokyo.jp/kitakuseiso/kurashi/gomi/bunbetsu/chirashi/gomi.html'
+
+  const body = await c.req.json<WebhookRequestBody>()
+  const event = body.events[0]
+
+  if (event.type !== 'message' || event.message.type !== 'text') return
+
+  await client.replyMessage({
+    replyToken: event.replyToken,
+    messages: [
+      {
+        type: 'text',
+        text: `今日は${today}\n明日は${tomorrow}\n\n${gomiUrl}`,
+      },
+    ],
+  })
+})
+
+export default {
+  fetch: app.fetch,
+}
