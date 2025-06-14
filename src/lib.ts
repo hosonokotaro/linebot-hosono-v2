122fb9b0e01602aab9b6176b341e6493d5746658
hosono kotaro <hosono1985@gmail.com>
2025-06-14 18:00:32 +0900

    Update lib.ts and test files (#12)

--- src/lib.ts
+++ src/lib.ts
@@ -20,10 +20,12 @@ export const getCurrentMonthWorkList = (baseDate: Date) => {
   } as const
 
   const result: string[] = []
-  let wednesdayCount = 0
 
-  for (let day = 0; day < end.getDate(); day++) {
-    const currentDayOfWeek = (start.getDay() + day) % 7
+  // NOTE: 実際の日付は dayCount を +1 した値
+  for (let dayCount = 0; dayCount < end.getDate(); dayCount++) {
+    const currentDayOfWeek = (start.getDay() + dayCount) % 7
+    const isFirstOrThirdWeekWednesday =
+      dayCount <= 6 || (dayCount >= 14 && dayCount <= 20)
 
     switch (currentDayOfWeek) {
       case WEEK.SUNDAY:
@@ -35,11 +37,8 @@ export const getCurrentMonthWorkList = (baseDate: Date) => {
         result.push('可燃ごみ')
         break
       case WEEK.WEDNESDAY:
-        wednesdayCount++
         // NOTE: 第1週と第3週の水曜日は不燃ごみ
-        result.push(
-          wednesdayCount === 1 || wednesdayCount === 3 ? '不燃ごみ' : '無し',
-        )
+        result.push(isFirstOrThirdWeekWednesday ? '不燃ごみ' : '無し')
         break
       case WEEK.THURSDAY:
         result.push('古紙、プラスチック')
@@ -58,8 +57,18 @@ export const getCurrentMonthWorkList = (baseDate: Date) => {
 export const getWasteScheduleMessage = (date: Date, env: EnvPublic): string => {
   const now = new Date(date.getTime() + 9 * 60 * 60 * 1000)
   const schedule = getCurrentMonthWorkList(now)
-  const today = schedule[now.getDate() - 1]
-  const tomorrow = schedule[now.getDate()]
+
+  // NOTE: todayNumber は 1 から始まることに留意する
+  const todayNumber = now.getDate()
+  let tomorrow = schedule[todayNumber]
+
+  if (todayNumber === schedule.length) {
+    const nextMonth = new Date(now.getFullYear(), now.getMonth() + 1, 1)
+    const nextMonthSchedule = getCurrentMonthWorkList(nextMonth)
+    tomorrow = nextMonthSchedule[0]
+  }
+
+  const today = schedule[todayNumber - 1]
 
   return `今日は${today}\n明日は${tomorrow}\n\n${env.URL_GOMI}`
 }
